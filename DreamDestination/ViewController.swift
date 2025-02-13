ceb87bf2b1fa7fe45005ebd8da1cd8f57432853f
Chron1um635 <chron1um522@gmail.com>
2025-02-13 18:30:17 +0300

    Initial Commit

---
+++ DreamDestination/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DreamDestination
+//
+//  Created by Максим Назаров on 13.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

2cf04a18930a88fd3fe13bb819fdf237b1952855
Chron1um635 <chron1um522@gmail.com>
2025-02-14 14:51:23 +0300

    Setup table view

--- DreamDestination/ViewControllers/DestinationsViewController.swift
+++ DreamDestination/ViewControllers/DestinationsViewController.swift
@@ -7,23 +7,78 @@
 
 import UIKit
 
-class DestinationsViewController: UIViewController {
-
+final class DestinationsViewController: UITableViewController {
+    
+    private let hotCountries = Country.getCountries().filter { $0.climate == .hot}
+    private let coldCountries = Country.getCountries().filter { $0.climate == .cold}
+    private let temperateCountries = Country.getCountries().filter { $0.climate == .temperate}
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.rowHeight = 280
+    }
 
-        // Do any additional setup after loading the view.
+}
+
+// MARK: - UITableViewDataSource
+extension DestinationsViewController {
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        3
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            return hotCountries.count
+        case 1:
+            return coldCountries.count
+        default:
+            return temperateCountries.count
+        }
     }
-    */
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "destinationCell", for: indexPath)
+        guard let destinationCell = cell as? DestinationCell else { return cell}
+        switch indexPath.section {
+        case 0:
+            destinationCell.configure(with: hotCountries[indexPath.row])
+        case 1:
+            destinationCell.configure(with: coldCountries[indexPath.row])
+        default :
+            destinationCell.configure(with: temperateCountries[indexPath.row])
+        }
+        
+        return destinationCell
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0:
+            return Climate.hot.rawValue
+        case 1:
+            return Climate.cold.rawValue
+        default:
+            return Climate.temperate.rawValue
+        }
+    }
+}
 
+
+// MARK: - Routing
+
+extension DestinationsViewController {
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let detailsVC = segue.destination as? DestinationDetailsViewController else { return }
+        guard let indexPath = tableView.indexPathForSelectedRow else { return }
+        switch indexPath.section {
+        case 0:
+            detailsVC.country = hotCountries[indexPath.row]
+        case 1:
+            detailsVC.country = coldCountries[indexPath.row]
+        default :
+            detailsVC.country = temperateCountries[indexPath.row]
+        }
+    }
 }
